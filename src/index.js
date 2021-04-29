ce78cb492c736bd0065403d7a87d0c803052615b
Hamilton <prof.hamiltonmachiti@usjt.br>
2021-04-29 10:43:06 -0300

    Inclusão do .dockerignore e Dockerfile

--- src/index.js
+++ src/index.js
@@ -11,7 +11,41 @@ const funcoes = {
             observacao.texto.includes(palavraChave) ?
             "importante" :
             "comum";
-        axios.post('http://localhost:10000/eventos', {
+        axios.post('http://192.168.200.100:10000/eventos', {
+            tipo: "ObservacaoClassificada",
+            dados: observacao,
+        }).catch((err) => {
+            console.log("err", err);
+        });
+    },
+};
+
+app.post('/eventos', (req, res) => {
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
+    res.status(200).send({
+        msg: "ok"
+    });
+});
+
+app.listen(7000, () => console.log("Classificação. Porta 7000"));
+
+/*const express = require("express");
+const axios = require("axios")
+const app = express();
+app.use(express.json());
+
+const palavraChave = "importante";
+
+const funcoes = {
+    ObservacaoCriada: (observacao) => {
+        observacao.status =
+            observacao.texto.includes(palavraChave) ?
+            // (observacao.idade >= palavraChave) ?
+            "importante" :
+            "comum";
+        axios.post('http://192.168.200.100:10000/eventos', {
             tipo: "ObservacaoClassificada",
             dados: observacao,
         });
@@ -25,4 +59,4 @@ app.post('/eventos', (req, res) => {
     res.status(200).send({ msg: "ok" });
 });
 
-app.listen(7000, () => console.log("Classificação. Porta 7000"));
+app.listen(7000, () => console.log("Classificação. Porta 7000"));*/
