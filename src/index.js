1ea1c8a31b1c3571df504288ba91884603bb5a9d
Hamilton <prof.hamiltonmachiti@usjt.br>
2021-04-29 10:49:21 -0300

    .

--- src/index.js
+++ src/index.js
@@ -30,33 +30,3 @@ app.post('/eventos', (req, res) => {
 });
 
 app.listen(7000, () => console.log("Classificação. Porta 7000"));
-
-/*const express = require("express");
-const axios = require("axios")
-const app = express();
-app.use(express.json());
-
-const palavraChave = "importante";
-
-const funcoes = {
-    ObservacaoCriada: (observacao) => {
-        observacao.status =
-            observacao.texto.includes(palavraChave) ?
-            // (observacao.idade >= palavraChave) ?
-            "importante" :
-            "comum";
-        axios.post('http://192.168.200.100:10000/eventos', {
-            tipo: "ObservacaoClassificada",
-            dados: observacao,
-        });
-    },
-};
-
-app.post('/eventos', (req, res) => {
-    try {
-        funcoes[req.body.tipo](req.body.dados);
-    } catch (err) {}
-    res.status(200).send({ msg: "ok" });
-});
-
-app.listen(7000, () => console.log("Classificação. Porta 7000"));*/
